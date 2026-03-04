169468ee22bef2ed2a059052caab25ce956979e8
NagKi-Tynlan <222875143+NagKi-Tynlan@users.noreply.github.com>
2026-03-04 10:32:22 -0700

    Tried to add another animation
    Tried to add another animation BUT NOT DONE. The click e animation

---
+++ main.js
@@ -0,0 +1,6 @@
+document.addEventListener("keydown",event=>{
+    console.log('Key down= ${event.key}') ; 
+});
+document.addEventListener("keyup",event=>{
+    console.log('Key up= ${event.key}') ; 
+});
